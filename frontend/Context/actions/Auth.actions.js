f765dfcd2ba977d5f4b493ece151d8a5c80ee645
Yize Zhao <yizezhao777@gmail.com>
2022-12-01 12:07:39 -0500

    first commit

---
+++ frontend/Context/actions/Auth.actions.js
@@ -0,0 +1,66 @@
+import jwt_decode from "jwt-decode"
+import AsyncStorage from "@react-native-community/async-storage"
+import Toast from "react-native-toast-message"
+import baseURL from "../../assets/common/baseUrl"
+
+export const SET_CURRENT_USER = "SET_CURRENT_USER";
+
+export const loginUser = (user, dispatch) => {
+    fetch(`${baseURL}users/login`, {
+        method: "POST",
+        body: JSON.stringify(user),
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+        },
+    })
+    .then((res) => res.json())
+    .then((data) => {
+        if (data) {
+            console.log(data)
+            const token = data.token;
+            AsyncStorage.setItem("jwt", token)
+            const decoded = jwt_decode(token)
+            console.log("here1")
+            dispatch(setCurrentUser(decoded, user))
+            console.log("here2")
+        } else {
+           logoutUser(dispatch)
+        }
+    })
+    .catch((err) => {
+        Toast.show({
+            topOffset: 60,
+            type: "error",
+            text1: "Please provide correct credentials",
+            text2: ""
+        });
+        logoutUser(dispatch)
+    });
+};
+
+export const getUserProfile = (id) => {
+    fetch(`${baseURL}users/${id}`, {
+        method: "GET",
+        body: JSON.stringify(user),
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json"
+        },
+    })
+    .then((res) => res.json())
+    .then((data) => console.log("data!!"));
+}
+
+export const logoutUser = (dispatch) => {
+    AsyncStorage.removeItem("jwt");
+    dispatch(setCurrentUser({}))
+}
+
+export const setCurrentUser = (decoded, user) => {
+    return {
+        type: SET_CURRENT_USER,
+        payload: decoded,
+        userProfile: user
+    }
+}
---
+++ frontend/Navigators/CartNavigator.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { createStackNavigator } from "@react-navigation/stack"
+
+import Cart from '../Screens/Cart/Cart';
+import CheckoutNavigator from './CheckoutNavigator';
+import Login from '../Screens/User/Login'
+
+const Stack = createStackNavigator();
+
+function MyStack() {
+    return(
+        <Stack.Navigator>
+            <Stack.Screen 
+                name="Cart"
+                component={Cart}
+                options={{
+                    headerShown: false
+                }}
+            />
+            <Stack.Screen 
+                name="Checkout"
+                component={CheckoutNavigator}
+                options={{
+                    title: 'Checkout'
+                }}
+            />
+            <Stack.Screen 
+                name="Login"
+                component={Login}
+                options={{
+                    headerShown: false
+                }}
+            />
+        </Stack.Navigator>
+    )
+}
+
+export default function CartNavigator() {
+    return <MyStack />
+}
